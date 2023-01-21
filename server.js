57fedf55132cf7c6dba30d7744acdd5861d22af3
Ploszajf <72620915+Ploszajf@users.noreply.github.com>
2023-01-21 01:05:54 +0100

    hostname

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const http = require("http")
 
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000
 
 const app = express()
